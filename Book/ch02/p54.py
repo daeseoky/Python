a8073c82004b54dcac83fa4ffbc11b53e8182e17
unknown <cesiya3@gmail.com>
2022-01-04 18:22:10 +0900

    2022/01/04 수업예제

---
+++ Book/ch02/p54.py
@@ -0,0 +1,12 @@
+# escape 문자 적용
+print('escape문자 차단')
+print('\n출력 scape문자 ')   #\n 줄바꿈 가능
+
+# escape 문자 기능 차단
+print('\\n출력 이스케이프 기능 차단1')
+print(r'\n출력 이스케이프 기능 차단2')
+
+# 경로 표현 : C:\Python\test
+print('path = ', 'C:\Python\test')
+print('path = ', 'C:\\Python\\test')
+print('path = ', r'C:\Python\test')
