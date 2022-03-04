9466f6e7c4b4bbed0684ab2aa4c6888766ddcb6d
unknown <cesiya3@gmail.com>
2022-03-04 18:19:18 +0900

    2022/03/04

--- Book/ch05/p133.py
+++ Book/ch05/p133.py
@@ -17,6 +17,28 @@ b = a()  # 외부 함수 호출 : a 함수
 b()      # 내부 함수 호출 : b 함수
 
 
-# (1) 함수 클로져
+# (2) 함수 클로져
+data = list(range(1, 101))
+def outer_func(data):
+    dataSet = data  # 값(1~100) 생성
+
+    # inner
+    def tot():
+        tot_val = sum(dataSet)
+        return tot_val
+    def avg(tot_val):
+        avg_val = tot_val / len(dataSet)
+        return avg_val
+
+    return tot, avg   # inner 반환
+
+# 외부 함수 호출 : data 생성
+tot, avg = outer_func(data)
+
+# 내부 함수 호출
+tot_val = tot()
+print('tot :', tot_val)
+avg_val = avg(tot_val)
+print('avg :', avg_val)
 
 
